327c96dc83d177e40993f71d6af75f53daf4c301
vhmaciel <vhsfmaciel@inf.ufrgs.br>
2021-06-30 14:51:13 -0300

    .

--- src/cell.h
+++ src/cell.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 
 enum TransType {PMOS, NMOS};
@@ -10,5 +11,15 @@ struct Transistor{
     int drain, gate, source;
 	TransType type;
 	float length, width;	
+
+};
+
+class Cell {
+
+
+	private:
+		std::string cellName; 
+		std::vector<Transistor> trans;
+
 
 };
