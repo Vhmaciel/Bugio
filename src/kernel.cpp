b68531b6127f678e78d274d1b6c9f1306ff5feb8
vhmaciel <vhsfmaciel@inf.ufrgs.br>
2021-07-07 12:10:25 -0300

    .

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -2,15 +2,40 @@
 #include "kernel.h"
 
 
-void Kernel::readSpice(std::string spiceNetlistFile){
+void Kernel::readSpice(fs::path filePath){
 
-    std::ofstream SpiceFile;
+    std::ifstream spiceFile;
+    std::string line, extension;
 
-    SpiceFile.open(spiceNetlistFile);
+    extension = filePath.extension();
 
-    
+    try{
+        if(extension == ".sp" || extension == ".cir" || extension == ".spc" || extension == ".spi" || extension == ".sps" || extension == ".inc" || extension == ".lib"){
 
-    SpiceFile.close();
+        }else
+            throw;
+
+    }catch(...){
+        std::cout <<  "ERROR: File extension is not valid" << std::endl;
+
+    }
+
+
+    spiceFile.open(filePath);
+
+    if(!spiceFile.is_open()){
+        std::cout << "ERROR: File opening error" << std::endl;      //usar outra classe posteriormente para tratamento de erros
+        return;
+    }
+
+    while(getline (spiceFile,line)){
+
+
+
+    }
+
+
+    spiceFile.close();
 
 
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <experimental/filesystem> //C++ 17
 #include <string>
 #include "cell.h"
+namespace fs = std::experimental::filesystem;
 
 
 class Kernel {
 
     public:
-        void readSpice(std::string spiceNetlistFile);
+        void readSpice(fs::path filePath);
 
     private:
         
